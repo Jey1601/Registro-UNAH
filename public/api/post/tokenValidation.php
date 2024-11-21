f4f3ac8cbc6211eb6cdf2838990a1195e0278dc5
angel-513 <angelnolasco513@gmail.com>
2024-11-20 20:48:05 -0600

    feat:validacion de credeciales precarga de admission admin

---
+++ public/api/post/tokenValidation.php
@@ -0,0 +1,33 @@
+<?php
+include_once '../../../src/DAO/util/jwt.php';
+
+header("Content-Type: application/json");
+
+$data = json_decode(file_get_contents('php://input', true));
+
+if (!isset($data['token']) || empty($data['token'])) {
+    echo json_encode([
+        'success' => false,
+        'message' => 'Token nulo.'
+    ]);
+    exit;
+} else {
+    $token = $data['token'];
+    $response = JWT::validateToken($token);
+
+    if ($response === false) {
+        echo json_encode([
+            'success' => false,
+            'message' => 'Token invalido.'
+        ]);
+        exit;
+    } else {
+        echo json_encode([
+            'success' => true,
+            'message' => 'Validacion exitosa.',
+            'payload' => $response
+        ]);
+    }
+}
+
+?>
