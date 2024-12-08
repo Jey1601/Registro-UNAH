5263699fff42f8f0d5d86a43637c16f5b5abbc79
aljosemoya <138126068+aljosemoya@users.noreply.github.com>
2024-12-08 11:47:05 -0600

    Add files via upload
    API DE GET DATA ACADEMIC PLANNING

---
+++ public/api/get/academicPlanning/getDataAcademicSchedulesAcademicPlanning.php
@@ -0,0 +1,31 @@
+<?php
+
+header("Content-Type: application/json");
+
+$path = '../../../../';
+include_once $path . "src/DAO/DataAcademicSchedulesAcademicPlanningDAO.php";
+
+$daoDataAcademicSchedulesAcademicPlanning = new DataAcademicSchedulesAcademicPlanningDAO();
+
+try {
+    $result = $daoDataAcademicSchedulesAcademicPlanning->getDataAcademicSchedulesAcademicPlanning();
+
+    if ($result['status'] === 'success') {
+        echo json_encode([
+            "status" => "success",
+            "data" => $result['data'] 
+        ]);
+    } else {
+        echo json_encode([
+            "status" => "error",
+            "message" => "Ocurrio un error en: getDataAcademicSchedulesAcademicPlanning()"
+        ]);
+    }
+} catch (Exception $e) {
+    echo json_encode([
+        "status" => "error",
+        "message" => "Ocurrio un error en: getDataAcademicSchedulesAcademicPlanning(): " . $e->getMessage()
+    ]);
+}
+
+?>
