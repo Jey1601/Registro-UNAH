6a5ce8b1c3d17c52bd6cb1e53a993e8d98f1329b
keniarn <79380340+keniarn@users.noreply.github.com>
2024-12-10 23:58:21 -0600

    Add files via upload

---
+++ db-scripts/ddl/CREATE_TABLES_PASSWORDRESETREQUEST.sql
@@ -0,0 +1,25 @@
+use unah_registration;
+
+-- @author TABLE PasswordResetRequestsStudents: Kenia Romero 20171003359  @created 5/12/2024
+CREATE TABLE PasswordResetRequestsStudents (
+    id_reset_request_student INT AUTO_INCREMENT PRIMARY KEY,
+    id_user_student INT NOT NULL,
+	token_id_student INT NOT NULL,
+    token_expiry_student DATETIME NOT NULL,
+    used_token_student BOOLEAN DEFAULT FALSE,
+    request_student_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_user_student) REFERENCES UsersStudents(id_user_student),
+    FOREIGN KEY (token_id_student) REFERENCES TokenUserStudent(id_token_user_student)
+);
+
+-- @author TABLE PasswordResetRequestsProfessors: Kenia Romero 20171003359  @created 9/12/2024
+CREATE TABLE PasswordResetRequestsProfessors (
+    id_reset_request_professor INT AUTO_INCREMENT PRIMARY KEY,
+    id_user_professor INT NOT NULL,
+	token_id_professor INT NOT NULL,
+    token_expiry_professor DATETIME NOT NULL,
+    used_token_professor BOOLEAN DEFAULT FALSE,
+    request_professor_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_user_professor) REFERENCES UsersProfessors(id_user_professor),
+    FOREIGN KEY (token_id_professor) REFERENCES TokenUserProfessor(id_token_user_professor)
+);
