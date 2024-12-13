961ad28b124d195ff6945b7c6d81ddcb56dd2048
aljosemoya <138126068+aljosemoya@users.noreply.github.com>
2024-12-12 23:20:16 -0600

    Update DistributionApplicantsByUserAdministrator.php
    DOCUMENTACION

--- public/api/get/admisionProcess/DistributionApplicantsByUserAdministrator.php
+++ public/api/get/admisionProcess/DistributionApplicantsByUserAdministrator.php
@@ -1,5 +1,8 @@
 <?php
-
+    /*
+    * @author Alejandro Moya 20211020462
+    * @created Noviembre de 2024
+    */
 header("Content-Type: application/json");
 $path = '../../../../';
 include_once $path."src/DAO/AdmissionAdminDAO.php";
